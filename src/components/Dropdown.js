224835a874658bc88be37784472d22fa6834cad1
Leprekus <leprekus@yahoo.com>
2022-11-26 18:52:56 +0900

    add authEndpoint

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,3 +1,19 @@
-export const Dropdown = () => {
-    return (null)
+export const Dropdown = ({ isDisplayed, searchTerm }) => {
+    console.log(searchTerm)
+    const secret = process.env.REACT_APP_REDDIT_SECRET
+    let 
+    CLIENT_ID = 'qjuHHDbFqllu2b76JMYmGQ',
+    RESPONSE_TYPE = 'code', //code for implicit grant
+    RANDOM_STRING,
+    URI,
+    DURATION,
+    SCOPE_STRING
+    const authEndpoint = `https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&
+    state=${RANDOM_STRING}&redirect_uri=${URI}&duration=${DURATION}&scope=${SCOPE_STRING}`
+    return (
+        <div className="dropdown-content" style={{display: isDisplayed}}>
+            <p>Hello World!
+            </p>
+        </div>
+    )
 }
--- src/routes/root.js
+++ src/routes/root.js
@@ -20,6 +20,7 @@ export default function Root () {
     // <li>advertise</li> removing
     // <li>shop avatars</li> removing
     // <li>profile pic</li> staying
+    const [searchTerm, setSearchTerm] = useState('')
     const [isDisplayed, setIsDisplayed] = useState('none')
     const [displaySearchbar, setDisplaySearchbar] = useState('none')
     const dropdownRef = useRef()
@@ -48,6 +49,10 @@ export default function Root () {
             if(window.innerWidth < 720) setDisplaySearchbar('none')
         }
     }
+
+    const handleSearchTerm = ({ target }) => {
+        setSearchTerm(target.value)
+    }
     
     return (
         <>
@@ -59,9 +64,9 @@ export default function Root () {
                     <label for="searchbar header">
                         <svg focusable="false" className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
                     </label>
-                    <input className="searchbar" style={{display: 'block'}} placeholder='Search Subreddits'/>
+                    <input className="searchbar" onChange={handleSearchTerm} style={{display: 'block'}} placeholder='Search Subreddits' value={searchTerm}/>
                 </form>
-                <Dropdown/>
+                <Dropdown isDisplayed={isDisplayed} searchTerm={searchTerm}/>
                 </NavLink>
 
                 <NavLink title="Popular" className='button' to='/samplePath' style={({ isActive }) => isActive ? active : notActive }><img className="icon" src={trendingIcon} alt='popular'/></NavLink>
